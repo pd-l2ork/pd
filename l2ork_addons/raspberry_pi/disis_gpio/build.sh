d1fd7121ef41059bdedad8515af873807d5e9c59
Ivica Ico Bukvic <ico@vt.edu>
2019-06-13 13:00:47 -0400

    *Fixed build of disis_gpio and removed building RPi externals that crash on non RPi hardware

--- l2ork_addons/raspberry_pi/disis_gpio/build.sh
+++ l2ork_addons/raspberry_pi/disis_gpio/build.sh
@@ -1,5 +1,7 @@
 curdir=`pwd`
-cd wiringPi/ && cd wiringPi/ && make static
+cd wiringPi/
+git reset --hard 96344ff7125182989f98d3be8d111952a8f74e15
+cd wiringPi/ && make static
 cd $curdir
 make
 exit 0
